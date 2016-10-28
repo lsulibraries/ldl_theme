8b9465574cbbd55c6cba5e146f7da2d37d6f119b
Kyle Tanglao <ktanglao@lsu.edu>
2016-10-28 10:39:10 -0500

    .landingMessageMobile added

--- ldltheme.js
+++ ldltheme.js
@@ -21,7 +21,7 @@
       window.sr = ScrollReveal();
       sr.reveal('.page-islandora-search .region-sidebar-first-inner', { duration: 200, delay: 100, opacity: 1, easing: 'linear', scale: 1, viewFactor: 0.01, }, 50);
       sr.reveal('#block-block-9',  { duration: 500, distance: '30px', opacity: 1, delay: 100,  easing: 'linear', scale: 1, viewFactor: 0.2, }, 50);
-      sr.reveal('.islandora-solr-search-result, .landingMessage', { duration: 200, delay: 100,  easing: 'linear', scale: 1, viewFactor: 0.01,}, 20);
+      sr.reveal('.islandora-solr-search-result', { duration: 200, delay: 100,  easing: 'linear', scale: 1, viewFactor: 0.01,}, 20);
       sr.reveal('.islandora-solr-sort, .landingHero', { duration: 200, delay: 0,  easing: 'linear', scale: 1, viewFactor: 0.01,}, 20);
       sr.reveal('.solr-fields, .islandora-solr-sort li, .page-browse-collections tr', { duration: 200, delay: 350,  easing: 'linear', scale: 1, viewFactor: 0.01, }, 20);
       sr.reveal('.solr-thumb img', { duration: 200, delay: 850,  easing: 'linear', scale: 1, viewFactor: 0.01, }, 20);
@@ -46,6 +46,7 @@
       $("#block-islandora-solr-simple").clone().prop({ id: "largeSearch", value:"Search LDL.."}).appendTo(".landingContainer");
       $("#block-block-1").clone().prop({ id: "landingAdvanced"}).appendTo("#largeSearch");
       $(".underHero").appendTo("#landingHero");
+      $(".landingMessage").clone().prop({id: "landingMobile", class: "landingMessageMobile"}).appendTo("#zone-header");
 
     //begin show more script
     var showChar = 300;  // How many characters are shown by default
@@ -54,7 +55,7 @@
     var lesstext = "Show less";
 
 
-    $('.short_desc p').each(function() {
+    $('.short_desc p, .mods-abstract-mt').each(function() {
         var content = $(this).html();
 
         if(content.length > showChar) {
