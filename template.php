37333961837eb78ba2675646e45991ebea616f1c
Jason Peak <jpeak5@lsu.edu>
2017-01-18 12:40:34 -0600

    hooks basic-collection-wrapper preprocess function to include item count; overrides corresponding template to include item count element

--- template.php
+++ template.php
@@ -10,3 +10,11 @@
  * for your subtheme grows. Please read the README.txt in the /preprocess and /process subfolders
  * for more information on this topic.
  */
+
+function alpha_preprocess_islandora_basic_collection_wrapper(&$variables){
+    $page_number = (empty($_GET['page'])) ? 0 : $_GET['page'];
+    $page_size = (empty($_GET['pagesize'])) ? variable_get('islandora_basic_collection_page_size', '12') : $_GET['pagesize'];
+    $islandora_object = $variables['islandora_object'];
+    list($total_count, $results) = islandora_basic_collection_get_member_objects($islandora_object, $page_number, $page_size);
+    $variables['total_count'] = $total_count;
+}
