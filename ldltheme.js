544263e61687ef7437fce728b7e1ec0a8d6165de
root <root@library.local>
2017-05-24 13:04:55 -0500

    Adds Loyola and mcneese

--- ldltheme.js
+++ ldltheme.js
@@ -23,13 +23,22 @@
         $("body").addClass('lsuTheme institution');
       };      
 
+      if (url.indexOf('/mcneese-') !== -1) {
+        $("body").addClass('mcneeseTheme institution');
+      };      
+
+      if (url.indexOf('/loyola-') !== -1) {
+        $("body").addClass('loyolaTheme institution');
+      };      
+
+
+
       $('#switchTheme').click(function(){
           $("html").toggleClass('Dark');
       });
 
       window.sr = ScrollReveal();
       sr.reveal('.page-islandora-search .region-sidebar-first-inner', { duration: 200, delay: 100, opacity: 1, easing: 'linear', scale: 1, viewFactor: 0.01, }, 50);
-      sr.reveal('#block-block-9',  { duration: 500, distance: '30px', opacity: 1, delay: 100,  easing: 'linear', scale: 1, viewFactor: 0.2, }, 50);
       sr.reveal('.islandora-solr-search-result, .landingMobile', { duration: 200, delay: 100,  easing: 'linear', scale: 1, viewFactor: 0.01,}, 20); //landingMessage normally here; conflicts with mobile landingMessage
       sr.reveal('.islandora-solr-sort, .landingHero', { duration: 200, delay: 0,  easing: 'linear', scale: 1, viewFactor: 0.01,}, 20);
       sr.reveal('.solr-fields, .islandora-solr-sort li, .page-browse-collections tr, .islandora-pdf-content', { duration: 200, delay: 350,  easing: 'linear', scale: 1, viewFactor: 0.01, }, 20);
