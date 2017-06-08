d19d177149d3f780fe27f9967cd5918110f4143e
root <root@library.local>
2017-06-08 15:57:32 -0500

    corrects state theme

--- ldltheme.js
+++ ldltheme.js
@@ -15,7 +15,7 @@
       });
 
       var url = window.location.pathname;
-      var namespaces = ['uno','loyno', 'mcneese', 'lsu', 'latech', 'hnoc'];
+      var namespaces = ['uno','loyno', 'mcneese', 'lsu', 'latech', 'hnoc', 'tul', 'state'];
 
       for(namespace in namespaces){
 	  ns = namespaces[namespace];
