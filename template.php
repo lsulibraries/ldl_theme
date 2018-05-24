e0f36b1a19686a482918e61e52ccdd6d7a6096b9
ktanglao <ktanglao@lsu.edu>
2018-05-24 10:53:20 -0500

    Kills new downloadlinks via filter in php, adds mobile styles

--- template.php
+++ template.php
@@ -6,7 +6,7 @@ function alpha_preprocess_islandora_large_image(&$variables) {
   $variables['downloads'] = array();
   module_load_include('inc', 'islandora', 'includes/datastream');
   foreach($variables['islandora_object'] as $ds) {
-    if(in_array($ds->id, array('RELS-EXT'))) {
+    if(in_array($ds->id, array('RELS-EXT', 'TN', 'MODS', 'DC', 'OBJ', 'JP2', 'TECHMD'))) {
       continue;
     }
     $variables['downloads'][$ds->id] = array(
