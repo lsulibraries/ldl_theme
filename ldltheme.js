7e7951c047fa9f8d0a2a73d55572d9d4fc6714a6
ktanglao <ktanglao@lsu.edu>
2018-09-20 13:28:08 -0500

    Carries over custom audio player styles, updates templates permissions

--- ldltheme.js
+++ ldltheme.js
@@ -74,6 +74,26 @@
           break;
         }
 
+        case ($('body').hasClass('audio')) :{
+          itemTitle = $(".modsTitle").html(); // finds full title without truncation
+          thumbnailURL = $(".image-thumbnail img").prop('src');
+          if ($('.block-islandora-compound-object').length){
+            compoundChild_start();
+          }
+          itemHeader();
+          typeClass('image');
+          imageContainer();
+          moveMetadata();
+          actionToggles();
+          itemFooter();
+          //pdfModal();
+          if ($('.block-islandora-compound-object').length){
+            compoundChild_end();
+          }
+          break;
+        }        
+
+
         case ($('body').hasClass('video')) :{
           itemTitle = $(".modsTitle").html(); // finds full title without truncation
           thumbnailURL = $(".image-thumbnail img").prop('src');
@@ -262,7 +282,7 @@
       function itemHeader(){
         if (!$('body').hasClass('compoundParent')){
           $(".region-inner > div.tabs.clearfix").prependTo("#block-system-main");
-          $("<div class='item_header'/>").insertBefore(".itemContainer, .islandora-video-object, .islandora-pdf-object, .islandora-large-image-object, .bookContainer, .islandora-newspaper-object"); //creates header for image items
+          $("<div class='item_header'/>").insertBefore(".itemContainer, .islandora-video-object, .islandora-audio-object, .islandora-pdf-object, .islandora-large-image-object, .bookContainer, .islandora-newspaper-object"); //creates header for image items
           thumbnailURL = $(".image-thumbnail img").prop('data-src');
           var sequenceText = $('span#islandora-compound-sequence-position').text();
           $('<div class="currentPart"></div>').html(sequenceText).appendTo('.headerBreadcrumb');
@@ -374,11 +394,14 @@
      }
 
       function imageContainer(){
-        $(".islandora-large-image-object, .islandora-newspaper-object, .islandora-pdf-object, .islandora-video-object").addClass("itemContainer imageContainer"); //adds label break
+        $(".islandora-large-image-object, .islandora-newspaper-object, .islandora-audio-object, .islandora-pdf-object, .islandora-video-object").addClass("itemContainer imageContainer"); //adds label break
         $("<div class='labelContainer'/>").insertBefore(".imageContainer"); //adds label break
         if ($('body').hasClass('pdf')){
           $("<div class='contentLabel imageLabel'>PDF Object</div>").appendTo(".labelContainer"); //adds label break
         }
+        if ($('body').hasClass('audio')){
+          $("<div class='contentLabel imageLabel'>Audio Object</div>").appendTo(".labelContainer"); //adds label break
+        }        
         else{
           $("<div class='contentLabel imageLabel'>Image Object</div>").appendTo(".labelContainer"); //adds label break        
         }        
@@ -819,7 +842,7 @@
 
 
       //oral history stuff
-      $("h1#page-title").clone().prop({ id: "oh-title", class: "ohtitle"}).prependTo(".islandora-audio-content");
+      //$("h1#page-title").clone().prop({ id: "oh-title", class: "ohtitle"}).prependTo(".islandora-audio-content");
       $(".modsContributor a").clone().prop({ class: "ohcreator"}).insertAfter(".ohtitle");
       $('a.ohcreator').wrapAll('<div class="creatorLinks"/>'); //wraps collectionPage title
       //end oral history
@@ -839,7 +862,7 @@
 
       // ===== following chunk needs cleanup, some legacy code within =====
 
-      $("#page-title").clone().prop({ id: "window-title", class: "object-title"}).prependTo("div.islandora-pdf-content, div.islandora-large-image-content"); // old pdf title
+      $("#page-title").clone().prop({ id: "window-title", class: "object-title"}).prependTo("div.islandora-audio-object, div.islandora-pdf-content, div.islandora-large-image-content"); // old pdf title
       $("a.islandora-pdf-link").appendTo(".islandora-pdf-content");
       $(".islandora-pdf-metadata").appendTo(".islandora-pdf-content");
       $("#islandora-solr-result-count").prop({ id: "header-result-count"}).appendTo("#breadcrumb"); //search results total
@@ -1151,7 +1174,7 @@ function monthClick(){
       }
       // end newspaper 2.0
 
-      if ( ($('body').hasClass('compoundObject')) || ($('body').hasClass('video')) || ($('body').hasClass('pdf')) || ($('body').hasClass('bookViewer')) || ($('body').hasClass('context-data')) || ($('body').hasClass('largeImage')) || ($('body').hasClass('newspaperSet'))){
+      if ( ($('body').hasClass('compoundObject')) || ($('body').hasClass('audio')) || ($('body').hasClass('video')) || ($('body').hasClass('pdf')) || ($('body').hasClass('bookViewer')) || ($('body').hasClass('context-data')) || ($('body').hasClass('largeImage')) || ($('body').hasClass('newspaperSet'))){
       $('body').addClass('headerversiontwo');
       }
       if (navigator.appName == 'Microsoft Internet Explorer' ||  !!(navigator.userAgent.match(/Trident/) || navigator.userAgent.match(/rv:11/)) || (typeof $.browser !== "undefined" && $.browser.msie == 1))
