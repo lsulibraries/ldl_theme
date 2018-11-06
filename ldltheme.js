4548d0228690cdd4f1a33a2d963ca178e15340bf
ktanglao <ktanglao@lsu.edu>
2018-11-06 15:43:10 -0600

    Hotfix to allow logic to continue without .modsTitle available to DOM

--- ldltheme.js
+++ ldltheme.js
@@ -440,7 +440,7 @@
 
         $("ul.tabs").appendTo(".userMenu:first").wrapAll('<div class="manageMenu"/>').insertBefore('#shareToggle'); //moves the view/ip embargo/manage menu
         $("div#block-system-main > div.tabs").remove(); // removes top div which once contained the tabs
-        if((itemTitle).length > 20) {
+        if (((itemTitle).length > 20) && ($(".modsTitle").length)) {
           $(".itemTitle").css('font-size','34px');
         }
      }
