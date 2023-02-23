56e60fbe7132dd35987cbc3774abded02026e600
flummingbird <wconli1@lsu.edu>
2023-02-23 14:42:29 +0000

    dgi-quickfix

--- ldltheme.js
+++ ldltheme.js
@@ -125,6 +125,8 @@
         case (($(".islandora-basic-collection-item-count").length) > 0) :{
           $("body").addClass("collectionPage");
           $(".collectionPage <div class='collectionSearch'/>").appendTo(".collectionHeader");
+          //$(".collectionPage <div class='collectionSearch'/>").appendTo(".collectionHeader");
+          $("<div class='collectionSearch'></div>").appendTo(".collectionHeader");
           $(".collectionPage #block-islandora-collection-search-islandora-collection-search").appendTo(".collectionSearch");
           $(".collectionSearch input.form-submit").val(' ');
           if ($('body').hasClass('page-islandora-object-islandoraroot')) {
